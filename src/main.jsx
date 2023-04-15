74ed014fc9a73d4a0d7d675b4163b23e10f83d9d
Ilya Hancharuk <innot707@gmail.com>
2023-04-15 13:01:09 +0300

    feat: implement favorite contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoutesFromElements, RouterProvider, createBrowserRouter, Route } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
 import Root, { rootAction, rootLoader } from "./routes/root";
-import Contact, { contactLoader } from "./routes/contact";
+import Contact, { contactAction, contactLoader } from "./routes/contact";
 import EditContact, { editAction } from "./routes/edit";
 import { destroyAction } from "./routes/destroy";
 import Index from "./routes";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        loader: rootLoader,
-        action: rootAction,
-        children: [
-            {
-                index: true,
-                element: <Index />,
-            },
-            {
-                path: "contacts/:contactId",
-                element: <Contact />,
-                loader: contactLoader,
-            },
-            {
-                path: "contacts/:contactId/edit",
-                element: <EditContact />,
-                loader: contactLoader,
-                action: editAction,
-            },
-            {
-                path: "contacts/:contactId/destroy",
-                action: destroyAction,
-                errorElement: <div>Oops! There was an error.</div>,
-            },
-        ],
-    },
-]);
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Root />} loader={rootLoader} action={rootAction} errorElement={<ErrorPage />}>
+            <Route errorElement={<ErrorPage />}>
+                <Route index element={<Index />} />
+                <Route path="contacts/:contactId" element={<Contact />} loader={contactLoader} action={contactAction} />
+                <Route
+                    path="contacts/:contactId/edit"
+                    element={<EditContact />}
+                    loader={contactLoader}
+                    action={editAction}
+                />
+                <Route path="contacts/:contactId/destroy" action={destroyAction} />
+            </Route>
+        </Route>
+    )
+);
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Root />,
+//         errorElement: <ErrorPage />,
+//         loader: rootLoader,
+//         action: rootAction,
+//         children: [
+//             {
+//                 errorElement: <ErrorPage />,
+//                 children: [
+//                     {
+//                         index: true,
+//                         element: <Index />,
+//                     },
+//                     {
+//                         path: "contacts/:contactId",
+//                         element: <Contact />,
+//                         loader: contactLoader,
+//                         action: contactAction,
+//                     },
+//                     {
+//                         path: "contacts/:contactId/edit",
+//                         element: <EditContact />,
+//                         loader: contactLoader,
+//                         action: editAction,
+//                     },
+//                     {
+//                         path: "contacts/:contactId/destroy",
+//                         action: destroyAction,
+//                         errorElement: <div>Oops! There was an error.</div>,
+//                     },
+//                 ],
+//             },
+//         ],
+//     },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,10 +1,23 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function contactLoader({ params }) {
-  const contact = await getContact(params.contactId);
-  return { contact };
+    const contact = await getContact(params.contactId);
+    if (!contact) {
+      throw new Response("", {
+        status: 404,
+        statusText: "Not Found",
+      });
+    }
+    return { contact };
 }
+
+export async function contactAction({ request, params }) {
+    let formData = await request.formData();
+    return updateContact(params.contactId, {
+      favorite: formData.get("favorite") === "true",
+    });
+  }
 
 export default function Contact() {
     const { contact } = useLoaderData();
@@ -59,10 +72,12 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+    const fetcher = useFetcher();
     // yes, this is a `let` for later
     let favorite = contact.favorite;
+    if (fetcher.formData) favorite = fetcher.formData.get("favorite") === "true";
     return (
-        <Form method="post">
+        <fetcher.Form method="post">
             <button
                 name="favorite"
                 value={favorite ? "false" : "true"}
@@ -70,6 +85,6 @@ function Favorite({ contact }) {
             >
                 {favorite ? "★" : "☆"}
             </button>
-        </Form>
+        </fetcher.Form>
     );
 }
